fe83542091b5af0945bd56a3719f4c7f1fcd56dc
kevinneo <351075349@qq.com>
2016-08-08 15:10:41 +0800

    Initial Commit

---
+++ XZCategories-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XZCategories-Demo
+//
+//  Created by niekaihua on 16/8/8.
+//  Copyright © 2016年 xiaozhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XZCategories-Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XZCategories-Demo
+//
+//  Created by niekaihua on 16/8/8.
+//  Copyright © 2016年 xiaozhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
